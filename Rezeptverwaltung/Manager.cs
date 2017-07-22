6c8c3fea16ff567b7c881e38d14a8b2c6bd2864f
Helexyastis <Helexyastis@users.noreply.github.com>
2017-07-22 19:41:52 +0200

    start

---
+++ Rezeptverwaltung/Manager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Rezeptverwaltung
+{
+    class Manager
+    {
+        string pathToRecipes = @"D:\VSProjects\Rezeptverwaltung\Recipes.xml";
+
+        XmlDocument document;
+        public Manager()
+        {
+            if (!System.IO.File.Exists(pathToRecipes))
+            {
+                System.IO.StreamWriter writer = System.IO.File.CreateText(pathToRecipes);
+                writer.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+                writer.WriteLine("<recipes>");
+                writer.WriteLine("</recipes>");
+                writer.Close();
+            }
+            document = new XmlDocument();
+            document.Load(pathToRecipes);
+            
+        }
+    }
+}
---
+++ Rezeptverwaltung/Recipe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rezeptverwaltung
+{
+    class Recipe
+    {
+        string name;
+        Dictionary<int,string> ingredients;
+        Dictionary<int, float> amounts;
+        Dictionary<int, string> units;
+
+        public Recipe(string name)
+        {
+            this.name = name;
+            ingredients = new Dictionary<int, string>();
+            amounts = new Dictionary<int, float>();
+            units = new Dictionary<int, string>();
+        }
+
+        public void AddIngredient(string name)
+        {
+            ingredients.Add(ingredients.Count, name);
+        }
+        public void AddAmount(float value)
+        {
+            amounts.Add(amounts.Count, value);
+        }
+        public void AddUnit(string name)
+        {
+            units.Add(units.Count, name);
+        }
+
+    }
+}
